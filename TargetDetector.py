e7b6cd85fbe62b7290272c88057910bdaba7010b
sfChen1029 <steven.fx.chen@gmail.com>
2017-10-23 19:20:35 -0700

    everything working for targetdetector except threshold values

--- TargetDetector.py
+++ TargetDetector.py
@@ -3,25 +3,28 @@ import numpy as np
 import math
 
 class TargetDetector:
-	image = ""
-	hsv_image = ""
-	def putImage(img):
+	#image
+	#hsv_image
+	def putImage(self, img):
+		global image
 		image = img
-		hsv_image = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-	def threshold():
-		THRESHOLD_MIN = np.array([150,240,95], np.uint8)
- 		THRESHOLD_MAX = np.array([250,255,220], np.uint8)
-		threshed_img = cv2.inRange(orig_img, THRESHOLD_MIN, THRESHOLD_MAX)
+		global hsv_image
+		hsv_image  = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+	def threshold(self):
+		THRESHOLD_MIN = np.array([0,0,0], np.uint8)
+		THRESHOLD_MAX = np.array([250,255,220], np.uint8)
+		global hsv_image
+		threshed_img = cv2.inRange(hsv_image, THRESHOLD_MIN, THRESHOLD_MAX)
 		return threshed_img
 
-cv2.namedWindow("Camera Feed", cv2.CV_WINDOW_AUTOSIZE)
+cv2.namedWindow("Camera Feed", cv2.WINDOW_AUTOSIZE)
 capture = cv2.VideoCapture(0)
-While True:
-	ret,frame = capture.read()
+while True:
+	ret, frame = capture.read()
 	targetDetector = TargetDetector()
 	targetDetector.putImage(frame)
 	threshed = targetDetector.threshold()
-	cv2.ShowImage("Camera Feed", threshed)
+	cv2.imshow("Camera Feed", threshed)
 	key = cv2.waitKey(10)
 	if key == 27:
 		cv2.destroyWindow("Camera Feed")
